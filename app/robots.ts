efef1409230fdb748584cdb7de2cce3672e81db8
babuas25 <babuas25@gmail.com>
2025-11-27 00:06:35 +0600

    Update robots.txt base URL format

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
-  const base = 'https://ridercritic.com'
+  const base = 'ridercritic.com'
 
   return {
     rules: {
